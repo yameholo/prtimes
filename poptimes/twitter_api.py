e42736b66c0a69b6a1871af8aa9c010e0951ffb2
yameholo <ame.aroid.xz5@gmail.com>
2019-06-07 12:57:20 +0900

    Add base function

---
+++ poptimes/twitter_api.py
@@ -0,0 +1,17 @@
+from typing import List, Dict
+
+
+def get_tweet_user(title: str) -> List[Dict[str, str]]:
+    """
+    {title}のことについて呟いた人のユーザー情報
+
+    :param title: (str) 記事のタイトル
+    :return: [
+        {
+            "name": "name"
+            "user_url": "profile url by twitter"
+            "icon_url": "profile image url by twitter"
+        }
+    ]
+    """
+    return [{"": ""}]
